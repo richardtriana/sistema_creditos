bfac40a6a5d360ad69c78c7c8a076317855b1b20
Isabella Salazar <is.salazar00@gmail.com>
2022-01-27 00:57:15 -0500

    Data is connected to the database

--- resources/js/components/boxes/CreateEditBox.vue
+++ resources/js/components/boxes/CreateEditBox.vue
@@ -1,73 +1,101 @@
 <template>
-	<div>
-		<div
-			class="modal fade"
-			id="boxModal"
-			tabindex="-1"
-			aria-labelledby="boxModalLabel"
-			aria-hidden="true"
-		>
-			<div class="modal-dialog">
-				<div class="modal-content">
-					<div class="modal-header">
-						<h5 class="modal-title" id="boxModalLabel">Sede</h5>
-						<button
-							type="button"
-							class="close"
-							data-dismiss="modal"
-							aria-label="Close"
-						>
-							<span aria-hidden="true">&times;</span>
-						</button>
-					</div>
-					<form>
-						<div class="modal-body">
-							<div class="form-group">
-								<label for="exampleInputEmail1">Saldo disponible</label>
-								<input
-									type="number"
-									step="any"
-									class="form-control"
-									id="exampleInputEmail1"
-									aria-describedby="emailHelp"
-								/>
-							</div>
-							<div class="form-group">
-								<label for="exampleFormControlSelect1">Sede</label>
-								<select class="form-control" id="exampleFormControlSelect1">
-									<option>1</option>
-									<option>2</option>
-									<option>3</option>
-									<option>4</option>
-									<option>5</option>
-								</select>
-							</div>
-							<div class="form-group">
-								<label for="anadir_saldo">Añadir saldo</label>
-								<input
-									type="number"
-									step="any"
-									class="form-control"
-									id="anadir_saldo"
-								/>
-							</div>
-						</div>
-						<div class="modal-footer">
-							<button
-								type="button"
-								class="btn btn-secondary"
-								data-dismiss="modal"
-							>
-								Close
-							</button>
-							<button type="submit" class="btn btn-primary">Submit</button>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
+  <div>
+    <div
+      class="modal fade"
+      id="boxModal"
+      tabindex="-1"
+      aria-labelledby="boxModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="boxModalLabel">Sede</h5>
+            <button
+              type="button"
+              class="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <form>
+            <div class="modal-body">
+              <div class="form-group">
+                <label for="exampleInputEmail1">Saldo disponible</label>
+                <input
+                  type="number"
+                  step="any"
+                  class="form-control"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  v-model="formBox.current_balance"
+                />
+              </div>
+              <div class="form-group">
+                <label for="exampleFormControlSelect1">Sede</label>
+                <select class="form-control" id="exampleFormControlSelect1">
+                  <option
+                    v-for="headquarter in headquarterList"
+                    :key="headquarter.id"
+                  >
+                    {{ headquarter.headquarter }}
+                  </option>
+                </select>
+              </div>
+              <div class="form-group">
+                <label for="anadir_saldo">Añadir saldo</label>
+                <input
+                  type="number"
+                  step="any"
+                  class="form-control"
+                  id="anadir_saldo"
+                />
+              </div>
+            </div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      editar: false,
+      formBox: {},
+      headquarterList: {},
+    };
+  },
+  created() {
+    this.listHeadquarters(1);
+  },
+  methods: {
+    showEditBox(box) {
+      this.editar = true;
+      let me = this;
+      $("#formBoxModal").modal("show");
+      me.formBox = box;
+      console.log(box);
+    },
+    listHeadquarters() {
+      let me = this;
+      axios.get(`api/headquarters/list-headquarter`).then(function (response) {
+        me.headquarterList = response.data;
+      });
+    },
+  },
+};
 </script>
