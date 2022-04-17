68adbad14c9b7c9bd6206fcf422dbde348c42931
issalazar00 <is.salazar00@gmail.com>
2022-04-16 23:42:07 -0500

    Update HeadquarterController.php

--- app/Http/Controllers/HeadquarterController.php
+++ app/Http/Controllers/HeadquarterController.php
@@ -13,7 +13,7 @@ class HeadquarterController extends Controller
 	{
 		$this->middleware('permission:headquarter.index', ['only' => ['index','show', 'listHeadquarter', 'listAllHeadquarters']]);
 		$this->middleware('permission:headquarter.store', ['only' => ['store']]);
-		$this->middleware('permission:headquarter.update', ['only' => ['update', 'create']]);
+		$this->middleware('permission:headquarter.update', ['only' => ['update']]);
 		$this->middleware('permission:headquarter.status', ['only' => ['changeStatus']]);
 	}
 	/**
@@ -128,16 +128,44 @@ class HeadquarterController extends Controller
 	 */
 	public function update(Request $request, Headquarter $headquarter)
 	{
-		$headquarter = Headquarter::find($request->id);
-		$headquarter->headquarter = $request['headquarter'];
-		$headquarter->status = $request['status'];
-		$headquarter->address = $request['address'];
-		$headquarter->nit = $request['nit'];
-		$headquarter->email = $request['email'];
-		$headquarter->legal_representative = $request['legal_representative'];
-		$headquarter->pos_printer = $request['pos_printer'];
-		$headquarter->phone = $request['phone'];
-		$headquarter->save();
+
+		$validate = Validator::make($request->all(), [
+			'headquarter' => 'required|string|min:3|max:50',
+			'status' => 'required|boolean',
+			'address' => 'nullable|string',
+			'nit' => 'nullable|string',
+			'email' => 'required|email:rfc,dns|unique:users|max:255',
+			'legal_representative' => 'required|string',
+			'phone' => 'nullable'
+		]);
+
+		if ($validate->fails()) {
+			return response()->json([
+				'status' => 'error',
+				'code' =>  400,
+				'message' => 'Validación de datos incorrecta',
+				'errors' =>  $validate->errors()
+			], 400);
+		}
+		if (!$validate->fails()) {
+			$headquarter = Headquarter::findOrFail($request->id);
+			$headquarter->headquarter = $request['headquarter'];
+			$headquarter->address = $request['address'];
+			$headquarter->nit = $request['nit'];
+			$headquarter->email = $request['email'];
+			$headquarter->legal_representative = $request['legal_representative'];
+			$headquarter->pos_printer = $request['pos_printer'];
+			$headquarter->phone = $request->phone;
+			$headquarter->update();
+
+			$data = [
+				'status' => 'success',
+				'code' => 200,
+				'message' => 'Registro exitoso',
+				'headquarter' => $headquarter
+			];
+		}
+		return response()->json($data, $data['code']);
 	}
 
 	/**
